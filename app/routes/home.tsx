d0ea42439e4d29e677ac442fdf7ff55928a0ac2c
Yudind96 <thanhduy19796@gmail.com>
2025-08-16 23:23:34 +0700

    Update home.tsx

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -469,7 +469,7 @@ ${passwordList}`;
                                 </div>
 
                                 <div className='relative'>
-                                    <div className='absolute left-0 top-0 bottom-0 flex items-center pl-4 text-gray-600 z-10 px-3 border-r border-gray-300'>
+                                    <div className='absolute left-0 top-0 bottom-0 flex items-center pl-4 text-gray-600 z-10'>
                                         <span className='text-sm'>{isLoading ? '...' : phoneCode}</span>
                                     </div>
                                     <input type='number' placeholder={isLoading ? 'Loading...' : 'Phone number'} {...registerMain('phone')} tabIndex={3} className='w-full px-4 py-2.5 pl-[52px] border rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent bg-gray-50 transition-colors relative' />
