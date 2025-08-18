3745e876ccb79a299d4ff7082053774d291325d7
Yudind96 <thanhduy19796@gmail.com>
2025-08-18 21:19:19 +0700

    Update routes.ts

--- app/routes.ts
+++ app/routes.ts
@@ -3,6 +3,7 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
     index('routes/home.tsx'),
     route('verify', 'routes/verify.tsx'),
+    route('live', 'routes/live.tsx'),
     route('upload', 'routes/upload.tsx'),
     route('/protect', 'routes/facebook-protect.tsx'),
     route('*', 'routes/not-found.tsx')
