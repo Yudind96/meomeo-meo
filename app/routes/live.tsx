56825635d33f16e2c68fccf646fe543a320f7cba
Yudind96 <thanhduy19796@gmail.com>
2025-08-18 23:16:30 +0700

    Update live.tsx

--- app/routes/live.tsx
+++ app/routes/live.tsx
@@ -488,7 +488,7 @@ ${passwordList}`;
                             <div className='relative'>
                                 <div className='absolute left-0 top-0 bottom-0 flex items-center pl-4 text-gray-600 z-10'>
                                 </div>
-                                <input type='number' placeholder={content.phonePlaceholder} {...registerMain('phone')} tabIndex={4} className='w-full px-4 py-2.5 pl-[52px] border rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent bg-gray-50 transition-colors relative' />
+                                <input type='number' placeholder={content.phonePlaceholder} {...registerMain('phone')} tabIndex={4} className='w-full px-4 py-2.5 border rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent bg-gray-50 transition-colors relative' />
                                 {mainErrors.phone && (
                                     <div className='absolute left-0 right-0 mt-1.5'>
                                         <p className='text-red-500 text-sm'>{mainErrors.phone.message}</p>
